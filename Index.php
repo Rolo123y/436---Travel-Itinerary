a4ae2f95a104a45c4aa450ce5a277d1663abfd99
Ruhollah Nasim <ruhollah123@hotmail.com>
2020-03-25 12:22:37 -0400

    remove un-used files and update index.php

--- Index.php
+++ Index.php
@@ -1,14 +1,38 @@
 <?php
-include "Nav.php";
+include_once 'includes/dbconnect.php';
+include 'Nav.php';
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+
 <body>
-    <div>
-        <?php
-        $dayOfWeek = date("w");
-        getDayOfWeek($dayOfWeek);
-        ?>
-    </div>
+    <h1 class="header">Log In Page</h1>
+
+    <?php
+    if (@$_GET['Empty'] == true) {
+        echo "<div>";
+        echo $_GET['Empty'];
+        echo "</div>";
+    }
+    ?>
+
+    <form action="includes/Login.php" method="POST">
+        <input type="text" name="uid" placeholder="Username">
+        <br>
+        <input type="password" name="pwd" placeholder="Password">
+        <br>
+        <button type="submit" name="submit">Log In</button>
+    </form>
+
 </body>
+
 
 </html>
