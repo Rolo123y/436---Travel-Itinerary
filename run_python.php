63047e6ba3eeb954009dc2c630c62406b81c8595
Gaurav Mudbhatkal <gigs612@gmail.com>
2020-03-25 10:51:06 -0400

    remove python runner

--- run_python.php
+++
@@ -1,9 +0,0 @@
-<?php
-// exec("cd data-collection");
-exec("python data-collection/flightscraper.py BOM DOH 20/05/2020",$outp,$return);
-// exec("data-collection/python -m hotelscraper.py  '2020/05/30' '2020/06/01'", $outp, $return);
-// exec('python test.py', $outp, $return);
-echo $return;
-print_r($outp);
-?>
-<!-- usage: tripadvisor_scraper.py [-h] checkin_date checkout_date sort locality -->
